ad2c45323f3cbf9f683b0a0b05b51c60512d4d42
0xfei <hhxlovety@gmail.com>
2016-12-26 10:59:39 +0800

    clear with index

--- mnesia/mnesia_opt.erl
+++ mnesia/mnesia_opt.erl
@@ -4,9 +4,10 @@
 -export([mnesia_start/0]).
 -export([create_table/0, create_table/2]).
 -export([insert_user/2]).
+-export([query_number/1]).
 -export([query_user/1]).
 
--record(user, {id, name, ctime}).
+-record(user, {id, name, phone_number, ctime}).
 
 mnesia_start() ->
     %mnesia:delete_schema([node()]),
@@ -16,15 +17,16 @@ mnesia_start() ->
 create_table() ->
     create_table(user, record_info(fields, user)).
 create_table(Name, Info) ->
-    mnesia:create_table(Name, [{index,[name]}, {attributes, Info}]).
+    mnesia:create_table(Name, [{index,[name]}, {attributes, Info}]),
+    mnesia:add_table_index(Name, #user.phone_number).
 
 -spec insert_user(integer(), list()) -> any().
 insert_user(Id, Name) ->
-    mnesia:dirty_write(user, #user{id=Id, name=Name, ctime=erlang:universaltime()}).
+    mnesia:dirty_write(user, #user{id=Id, name=Name, ctime=erlang:universaltime(), phone_number=12345}).
 
 -spec query_user(integer() | list()) -> {error, not_found_user} | any().
 query_user(Id) when is_integer(Id) ->
-    % wrong! index can just be used as secendary key.
+    % wrong! index can just be used as secondary key.
     Fun = fun() ->
         mnesia:index_read(user, Id, #user.id)
         end,
@@ -34,3 +36,6 @@ query_user(Name) when is_list(Name) ->
     mnesia:dirty_index_read(user, Name, #user.name);
 query_user(_Name) ->
     {error, not_found_user}.
+
+query_number(Number) ->
+    mnesia:dirty_index_read(user, Number, #user.phone_number).
